f6f16acadbaf6478ead253ca60ea26e8a8cf92e4
S-v-e-t <berezovskaya111@gmail.com>
2019-06-12 12:37:22 +0300

    remakks1

--- hw1/task3/task3.js
+++ hw1/task3/task3.js
@@ -1,13 +1,46 @@
+// const country = 'Напишите название страны в которую нужно доставить товар';
+// let message = prompt(country);
+// const cancel = 'Отменено пользователем!';
+// const noDelivery = 'В вашей стране доставка не доступна';
+// if (message === null) {
+//   message = cancel;
+// } else {
+//   const countryName = message;
+//   switch (message) {
+//     case 'китай':
+//       const credite = 100;
+//       message = `Доставка в ${countryName} будет стоить ${credite} кредитов`;
+//       break;
+//     case 'южная америка':
+//       const credite1 = 250;
+//       message = `Доставка в ${countryName} будет стоить ${credite1} кредитов`;
+//       break;
+//     case 'австралия':
+//       const credite2 = 170;
+//       message = `Доставка в ${countryName} будет стоить ${credite2} кредитов`;
+//       break;
+//     case 'индия':
+//       const credite3 = 80;
+//       message = `Доставка в ${countryName} будет стоить ${credite3} кредитов`;
+//       break;
+//     case 'ямайка':
+//       const credite4 = 120;
+//       message = `Доставка в ${countryName} будет стоить ${credite4} кредитов`;
+//       break;
+//     default:
+//       message = noDelivery;
+//   }
+// }
+// alert(message);
+
 const message = prompt(
   'Пожалуйста, укажите куда доставить товар: Китай, Южная Америка, Австралия, Индия, Ямайка',
 );
 
-
-
-if (message === null) { alert('Отменено пользователем'); }
-else {
- const messageUser = message.toLowerCase();
-
+if (message === null) {
+  alert('Отменено пользователем');
+} else {
+  const messageUser = message.toLowerCase();
 
   switch (messageUser) {
     case 'китай':
